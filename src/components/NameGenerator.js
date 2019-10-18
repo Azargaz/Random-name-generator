a22421e5f7677f1ea42f1028e9db01bdfadaa726
azargaz <huba310@gmail.com>
2019-10-18 13:33:16 +0200

    Now names are generated from two random names. NameGenerator fetches two random anime characters and takes parts of their names to comine into one name.

--- src/components/NameGenerator.js
+++ src/components/NameGenerator.js
@@ -3,35 +3,46 @@ import 'styles/NameGenerator.css';
 import Request from 'request';
 
 function NameGenerator() {
-	const [randomName, setRandomName] = useState("Random Name");
+	const [name, setName] = useState("Random");
+	const [surname, setSurname] = useState("Name");
 	const [loading, setLoading] = useState(false);
 
-	function requestRandomName() {
-		setLoading(true);
-		const id = Math.round(Math.random() * 100000);
-		Request(`https://kitsu.io/api/edge/anime-characters/${id}/character`, function (error, response, body) {
-			console.log('Status:', response.statusCode);
+	function createRandomName() {
+		const requestRandomName = (id, set) => {
+			setLoading(true);
+			Request(`https://kitsu.io/api/edge/anime-characters/${id}/character`, function (error, response, body) {
+				console.log('Status:', response.statusCode);
 
-			if (error) {
-				console.log(error);
-			} else {
-				try {
-					const newRandomName = JSON.parse(body)["data"]["attributes"]["canonicalName"];
-					setRandomName(newRandomName);
-				} catch (errors) {
-					console.log(errors);
+				if (error) {
+					console.log(error);
+				} else {
+					try {
+						const data = JSON.parse(body)["data"];
+						const fullName = data["attributes"]["canonicalName"].split(" ");
+						const randomName = fullName[Math.floor(Math.random() * fullName.length)];
+
+						console.log("Full name: " + fullName + " Random part: " + randomName);
+						set(randomName);
+						setLoading(false);
+					} catch (errors) {
+						console.log(errors);
+					}
 				}
-			}
+			});
+		}
 
-			setLoading(false);
-		});
+		const nameId = Math.round(Math.random() * 100000);
+		const surnameId = Math.round(Math.random() * 100000);
+
+		requestRandomName(nameId, setName);
+		requestRandomName(surnameId, setSurname);
 	}
 
 	return (
 		<>
-			<h3>{randomName}</h3>
-			<button className="Button" onClick={requestRandomName} disabled={loading}>Randomize</button>
-			<h3 hidden={!loading}>Loading...</h3>
+			<h3>{name + " " + surname}</h3>
+			<button className="Button" onClick={createRandomName} disabled={loading}>Randomize</button>
+			<div className={loading ? "loader" : "notloading"} />
 		</>
 	);
 }
