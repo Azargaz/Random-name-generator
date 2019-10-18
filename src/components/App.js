dfb5955a315660902ddab222148d729768311b5a
azargaz <huba310@gmail.com>
2019-10-18 10:54:33 +0200

    Fixed tab indentation.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import 'styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Random Name Generator</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
